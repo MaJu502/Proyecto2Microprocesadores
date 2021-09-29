d8a84061b8e7e01ed2a9e217b8941e182496072c
MarcoJurado20308 <71940438+MarcoJurado20308@users.noreply.github.com>
2021-09-28 22:21:35 -0600

    Funciona pero con race condition

--- ProyectoMircro.cpp
+++ ProyectoMircro.cpp
@@ -4,13 +4,11 @@
  * Proyecto Microprocesadores grupo X
  * integrantes: 
  *      Marco Jurado 20308
- *      Andrés de la Roca
+ *      Andrés de la Roca 20332
  *      José Auyón 201579
  * 
  *  Adaptado de method 4 geeksforgeeks
  **/
-
-//AGREGAR BARRERAS
 
  // Declaracion de librerias
 #include <bits/stdc++.h>
@@ -18,36 +16,19 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
+#include<unistd.h>
 using namespace std;
-
-//Declaracion de variables globales en hilos y usos de barreras y mutex
-#define F [2][2];
-pthread_mutex_t lock; // Bloqueo de hilos
-pthread_barrier_t barrera; //Barrera de los hilos
-
-
-void multiply(int F[2][2], int M[2][2]);
-
-void power(int F[2][2], int n);
  
-int fib(int n)
-{
-
-    //POSIBLe ubicacion 
-    
-    int F[2][2] = { { 1, 1 }, { 1, 0 } };
-    if (n == 0)
-        return 0;
-         
-    power(F, n - 1);
-     
-    return F[0][0];
-}
+//Declaracion de variables globales
+pthread_mutex_t lock1; // Bloqueo de hilos
+int result = 0;  //almacena resultado final
+int F[2][2] = { { 1, 1 }, { 1, 0 } };
+int M[2][2] = { { 1, 1 }, { 1, 0 } };
  
-void multiply(int F[2][2], int M[2][2])
+void *multiply(void *null)
 {
-    
-
+    pthread_mutex_lock(&lock1); //Bloqueo por mutex
     int x = F[0][0] * M[0][0] +
             F[0][1] * M[1][0];
     int y = F[0][0] * M[0][1] +
@@ -56,37 +37,25 @@ void multiply(int F[2][2], int M[2][2])
             F[1][1] * M[1][0];
     int w = F[1][0] * M[0][1] +
             F[1][1] * M[1][1];
-
-    pthread_mutex_lock(&lock);
-     
+    
+    //semaforo 
     F[0][0] = x;
     F[0][1] = y;
     F[1][0] = z;
     F[1][1] = w;
+    //semaforo
+    
 
-    pthread_mutex_unlock(&lock);
-    pthread_exit(0);
+    pthread_mutex_unlock(&lock1); //Desbloqueo por mutex
+    pthread_exit(0); //Salida
 }
  
-void power(int F[2][2], int n)
-{
-    int i;
-    int M[2][2] = { { 1, 1 }, { 1, 0 } };
-    
-    //pthread en main  simplificar power y multiply   --- reciban 
-    
-    for(i = 2; i <= n; i++){
-        err = pthread_create(&(tid[i]), NULL, &multiply(F,M), (void *)i);	//creacion de hilos con paso de parametros  ARREGLAR
-        //Posiblemente se necesita variables globales F y M VERIFICAR
-        if (err != 0)
-             printf("\ncan't create thread :[%s]", strerror(err));		//impresion de mensaje si el hilo no se crea correctamente													
-}
- 
+// Driver code
 int main()
-{
-    //Ingreso de numero a ejecutar -------------------------------------
+{//Ingreso de numero a ejecutar -------------------------------------
+    int err;
     int n = 0;
-    cout<< "Ingrese el numero a calcular se sucesion fibonacci: "
+    cout<< "Ingrese el numero a calcular se sucesion fibonacci: ";
     cin>>n;
     //------------------------------------------------------------------  
     
@@ -94,10 +63,23 @@ int main()
     pthread_t tid[n]; 
 
     //Execute a fib 
-    cout << " " <<  fib(n); //Imprime resultado
-    
-    pthread_mutex_destroy(&mutex);
+    if ( n == 1 || n == 2 ){
+        cout << " el resultado es: 1";
+    }
+    else if( n == 0 ){
+        cout << " el resultado es: 0";
+    }
+    else{
+        int i;
+        for(i = 0; i < n; i++){
+            err = pthread_create(&(tid[i]), NULL, multiply, NULL);
+            if (err != 0){
+                printf("\ncan't create thread :[%s]", strerror(err));
+            }
+        }
+        
 
-    return 0;
+        result = F[0][0];
+        cout << " el resultado es: " << result << endl;
+    }
 }
- 
